f190c3b57e89db9e34ccbef72c1acbfb985ceefd
Subhransu Sekhar Gouda <goudasubhransusekhar1@gmail.com>
2025-12-12 00:04:48 +0530

    Update spliting and joining a string.py

--- spliting and joining a string.py
+++ spliting and joining a string.py
@@ -1,6 +1,8 @@
+#This Programme Takes A String As Input And Performs Split And Join Operations On The String
 a=str(input("enter a string "))
 print(a)
 words=a.split(',')
 print(words)
 sen=" ".join(words)
 print(sen)
+
